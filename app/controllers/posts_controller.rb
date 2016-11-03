6b6c3039de2b1e58c80ec787f2a7ab8ee56e21a0
Ishan Subedi <ikushum@gmail.com>
2016-11-03 11:29:48 +0000

    added a rspec test for index action of post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
     end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
 end
