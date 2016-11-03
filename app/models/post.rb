ba3c5125e5c461eb2d27fe70dcdeeeed7cdbaa5b
Ishan Subedi <ikushum@gmail.com>
2016-11-03 12:40:09 +0000

    added test for create action for invalid attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
     validates :title, :author, :content, presence: true
-    validates :content, length: { minimum: 60 }, message: 'The content must have at least 60 characters.'
+    validates :content, length: { minimum: 60 }
 end
