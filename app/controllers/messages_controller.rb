db8fc96f107ced02146a520cb437925e003165a4
Ishan Subedi <ikushum@gmail.com>
2016-11-09 15:32:45 +0000

    defined create action for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+    def create 
+        @message = Message.create(message_params)
+        if @message.valid?
+            redirect_to root_path
+          else
+            redirect_to root_path(anchor: 'contact'))
+        end
+    end
+    
+    private
+    def message_params
+      params.require(:message).permit(:name, :email, :message, :created_at)
+    end
+    
 end
