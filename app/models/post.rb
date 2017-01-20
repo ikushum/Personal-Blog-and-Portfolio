49bdbaebd541b15e9d81fc867c8cf7213ac398f6
Ishan <ikushum@gmail.com>
2017-01-20 13:51:49 +0545

    frontend stuffs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ActiveRecord::Base
-	extend FriendlyId
-	friendly_id :title
     validates :title, :author, :content, :postimg, presence: true
     validates :content, length: { minimum: 60 }
     has_many :comments
