1cbd3fe5550e91c37bda978ced1c8717231dc991
Ishan Subedi <ikushum@gmail.com>
2017-02-03 08:26:51 +0000

    deleted old comment model and controller

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ActiveRecord::Base
-    belongs_to :post
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    validates :name, :comment, :email, :post_id, presence: true
-    validates :email , format: VALID_EMAIL_REGEX 
-end
